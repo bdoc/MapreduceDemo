47632479e27a867835eed3aa2e3a3daf5c8edebb
TobyHong <tobyhong@lagou.com>
2017-03-17 17:47:59 +0800

    Initial commit

---
+++ src/test/java/com/num3rd/mapreduce/demo/mos/WordCountTest.java
@@ -0,0 +1,21 @@
+package com.num3rd.mapreduce.demo.mos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pipe on 3/16/17.
+ */
+public class WordCountTest {
+    @Test
+    public void main() throws Exception {
+        String inDir = "/tmp/input";
+        String outDir = "/tmp/output/" + System.currentTimeMillis();
+        String[] args = new String[2];
+        args[0] = inDir;
+        args[1] = outDir;
+        WordCount.main(args);
+    }
+
+}
