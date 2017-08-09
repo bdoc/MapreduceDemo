5a04ba4884caa60698ad892835d5b633ed4d9619
Tobyhong <tobyhong@lagou.com>
2017-08-09 17:21:08 +0800

    MultipleOutputs.setCountersEnabled

--- src/main/java/com/num3rd/mapreduce/demo/mos/WordCount.java
+++ src/main/java/com/num3rd/mapreduce/demo/mos/WordCount.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
 import java.io.IOException;
@@ -21,11 +22,9 @@ public class WordCount {
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-        Counter counter;
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
-            counter = context.getCounter("mos", "counter");
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
@@ -48,7 +47,7 @@ public class WordCount {
                 sum += val.get();
             }
             result.set(sum);
-            mos.write("mos", key, result, key.toString());
+            mos.write("mos", key, result, result.toString());
         }
 
         @Override
@@ -71,6 +70,7 @@ public class WordCount {
         LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
 
         MultipleOutputs.addNamedOutput(job, "mos", TextOutputFormat.class, Text.class, IntWritable.class);
+        MultipleOutputs.setCountersEnabled(job, true);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
