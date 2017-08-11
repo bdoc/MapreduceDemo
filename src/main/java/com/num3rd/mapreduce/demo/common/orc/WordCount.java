45eeee37e542d578b34c8b12f738f7b2c2c6bcf1
Tobyhong <tobyhong@lagou.com>
2017-08-11 14:55:36 +0800

    OrcOutputFormat

---
+++ src/main/java/com/num3rd/mapreduce/demo/common/orc/WordCount.java
@@ -0,0 +1,89 @@
+package com.num3rd.mapreduce.demo.common.orc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.mapred.OrcList;
+import org.apache.orc.mapred.OrcStruct;
+import org.apache.orc.mapreduce.OrcOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Created by pipe on 3/15/17.
+ */
+public class WordCount {
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class combinerReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, NullWritable, OrcStruct> {
+        private TypeDescription schema = TypeDescription.fromString("struct<key:string,ints:array<int>>");
+        // createValue creates the correct value type for the schema
+        private OrcStruct pair = (OrcStruct) OrcStruct.createValue(schema);
+        // get a handle to the list of ints
+        private OrcList<IntWritable> valueList = (OrcList<IntWritable>) pair.getFieldValue(1);
+        private final NullWritable nada = NullWritable.get();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            pair.setFieldValue(0, key);
+            valueList.clear();
+            for(IntWritable val: values) {
+                valueList.add(new IntWritable(val.get()));
+            }
+            context.write(nada, pair);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("orc.mapred.output.schema","struct<key:string,ints:array<int>>");
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(combinerReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(OrcStruct.class);
+        job.setOutputFormatClass(OrcOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/test/java/com/num3rd/mapreduce/demo/common/orc/WordCountTest.java
@@ -0,0 +1,20 @@
+package com.num3rd.mapreduce.demo.common.orc;
+
+import org.junit.Test;
+
+/**
+ * Created by pipe on 3/16/17.
+ */
+public class WordCountTest {
+    @Test
+    public void main() throws Exception {
+        String HOME = System.getenv("HOME");
+        String inDir = HOME + "/input";
+        String outDir = HOME + "/output/" + System.currentTimeMillis();
+        String[] args = new String[2];
+        args[0] = inDir;
+        args[1] = outDir;
+        WordCount.main(args);
+    }
+
+}
