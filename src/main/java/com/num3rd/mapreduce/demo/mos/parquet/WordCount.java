b66469dc3ce0f66792ec8ddadcec4a337cfd9f54
Tobyhong <tobyhong@lagou.com>
2017-08-14 14:52:29 +0800

    AvroParquet

---
+++ src/main/java/com/num3rd/mapreduce/demo/mos/parquet/WordCount.java
@@ -0,0 +1,114 @@
+package com.num3rd.mapreduce.demo.mos.parquet;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.parquet.avro.AvroParquetOutputFormat;
+import org.apache.parquet.example.data.Group;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Created by pipe on 3/15/17.
+ */
+public class WordCount {
+    private static Schema SCHEMA = new Schema.Parser().parse("" +
+            "{\n" +
+            "   \"type\": \"record\",\n" +
+            "   \"name\": \"Word\",\n" +
+            "   \"fields\":[\n" +
+            "       {\"name\":\"word\", \"type\":\"string\"},\n" +
+            "       {\"name\":\"count\", \"type\":\"int\"}\n" +
+            "   ]\n" +
+            "}"
+    );
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class CombinerReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Void, GenericRecord> {
+        private GenericRecord record = new GenericData.Record(SCHEMA);
+        private MultipleOutputs mos;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            mos = new MultipleOutputs(context);
+        }
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+
+            record.put("word", key.toString());
+            record.put("count", sum);
+            String namedOutput = "mos" + sum;
+            mos.write(namedOutput, null, record, sum + "");
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            mos.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(CombinerReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+
+        for (int i = 1; i <= 3; i ++) {
+            String namedOutput = "mos" + i;
+            MultipleOutputs.addNamedOutput(job, namedOutput, AvroParquetOutputFormat.class, Void.class, Group.class);
+            AvroParquetOutputFormat.setSchema(job, SCHEMA);
+        }
+        MultipleOutputs.setCountersEnabled(job, true);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
